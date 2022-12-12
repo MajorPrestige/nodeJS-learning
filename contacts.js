083c2ebf570da8e83508c973472d713466faad74
Denys Dashkevych <denysdashkevych@gmail.com>
2022-12-12 14:52:56 +0200

    init db

---
+++ contacts.js
@@ -0,0 +1,31 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+ */
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
