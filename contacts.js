ad656302df13fad64fbafcac95d4aa69103ba1b7
Denys Dashkevych <denysdashkevych@gmail.com>
2022-12-12 17:09:51 +0200

    added getContacts

--- contacts.js
+++ contacts.js
@@ -4,16 +4,28 @@ const path = require("path");
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 const listContacts = async () => {
-  const data = await fs.readFile(contactsPath, "utf-8");
+  const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
 };
 
 const getContactById = async (contactId) => {
-  // ...твой код
+  const contacts = await listContacts();
+  const desiredContact = contacts.find((el) => el.id === contactId);
+
+  return desiredContact || null;
 };
 
 const removeContactById = async (contactId) => {
-  // ...твой код
+  const contacts = await listContacts();
+  const indexToRemove = contacts.findIndex((el) => el.id === contactId);
+
+  if (indexToRemove === -1) {
+    return null;
+  }
+
+  const [contactToRemove] = contacts.splice(indexToRemove, 1);
+
+  return contactToRemove;
 };
 
 const addContact = async (name, email, phone) => {
--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const contacts = require("./contacts");
 console.log(__dirname);
 
-
 const invokeActions = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "listContacts":
       const listContacts = await contacts.listContacts();
       console.log(listContacts);
       break;
+    case "getContactById":
+      const desiredContact = await contacts.getContactById(id);
+      console.log(desiredContact);
+      break;
+    case "removeContactById":
+      const contactToRemove = await contacts.removeContactById(id);
+      console.log(contactToRemove);
+      break;
+    case "addContact":
+      const contactToAdd = await contacts.addContact(name, email, phone);
+      console.log(contactToAdd);
+      break;
     default:
       console.log("Unknown action");
   }
 };
 
-invokeActions({ action: "listContacts" });
+// invokeActions({ action: "listContacts" });
+// invokeActions({ action: "getContactById", id: "1" });
+// invokeActions({ action: "removeContactById", id: "1" });
