10653a455325db3055e4c8be370237fef4d64d39
Denys Dashkevych <denysdashkevych@gmail.com>
2023-04-13 13:22:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 //! const yargs = require("yargs/yargs");
 //! const { hideBin } = require("yargs/helpers");
 
-const contacts = require("./contacts");
-const { program } = require("commander");
+const contacts = require('./contacts');
+const { program } = require('commander');
 
 const invokeActions = async ({ action, id, name, email, phone }) => {
   switch (action) {
-    case "list":
+    case 'list':
       const listContacts = await contacts.listContacts();
       console.log(listContacts);
       break;
 
-    case "get":
+    case 'get':
       const desiredContact = await contacts.getContactById(id);
       console.log(desiredContact);
       break;
 
-    case "remove":
+    case 'remove':
       const contactToRemove = await contacts.removeContactById(id);
       console.log(contactToRemove);
       break;
 
-    case "add":
+    case 'add':
       const contactToAdd = await contacts.addContact(name, email, phone);
       console.log(contactToAdd);
       break;
 
     default:
-      console.warn("\x1B[31m Unknown action type!");
+      console.warn('\x1B[31m Unknown action type!');
   }
 };
 
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
 
 program.parse();
 const options = program.opts();
